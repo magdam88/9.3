48149b86c3a645ce4e632895c40f21047bedfa35
Magda <Magda@Maczek.local>
2017-02-18 14:38:44 +0100

    v2

--- scripts.js
+++ scripts.js
@@ -1,21 +1,21 @@
 $(document).ready(function () {
     var carouselList = $("#carousel ul");
-    $(carouselList).setInterval(function () {
-            function changeSlide() {
-                $(carouselList).animate({
-                    'marginLeft': -400
-                }, 500, moveFirstSlide)
-            }
-        }, 3000),
 
-        function moveFirstSlide() {
-            var firstItem = carouselList.find("li:first");
-            var lastItem = carouselList.find("li:last");
-            lastItem.after(firstItem);
-            carouselList.css({
-                marginLeft: 0
-            });
+    function changeSlide() {
+        $(carouselList).animate({
+            'marginLeft': -400
+        }, 500, moveFirstSlide)
+    };
 
-        };
+    setInterval(changeSlide, 3000);
 
+    function moveFirstSlide() {
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");
+        lastItem.after(firstItem);
+        carouselList.css({
+            marginLeft: 0
+        });
+
+    };
 });
